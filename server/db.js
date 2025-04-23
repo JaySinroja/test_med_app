43867d694a7d489a9e93074197416eb5f9e850f1
JaySinroja <jay.sinroja12@gmail.com>
2025-04-24 00:42:38 +0530

    j

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb+srv://<jaysinroja12>:<jay1210>@cluster0.xxxxx.mongodb.net/?retryWrites=true&w=majority";
+// Replace this with your actual connection string
+const mongoURI = "mongodb+srv://jaysinroja12:jay1210@cluster0.vdtmgil.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
-
   
