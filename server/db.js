4260dd38598b90a2a4da0165ce66b3f2574c4ec8
JaySinroja <jay.sinroja12@gmail.com>
2025-04-24 00:38:27 +0530

    c

--- server/db.js
+++ server/db.js
@@ -1,28 +1,9 @@
-const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoose = require("mongoose");
 
-const connectToMongo = async (retryCount) => {
-    const MAX_RETRIES = 3;
-    const count = retryCount ?? 0;
-    try {
-        await mongoose.connect(mongoURI, { dbName: 'stayhealthybeta1'});
-        console.info('Connected to Mongo Successfully')
+const mongoURI = "mongodb+srv://<jaysinroja12>:<jay1210>@cluster0.xxxxx.mongodb.net/?retryWrites=true&w=majority";
 
-        return;
-    } catch (error) {
-        console.error(error);
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-        const nextRetryCount = count + 1;
-
-        if (nextRetryCount >= MAX_RETRIES) {
-            throw new Error('Unable to connect to Mongo!');
-        }
-
-        console.info(`Retrying, retry count: ${nextRetryCount}`)
-
-        return await connectToMongo(nextRetryCount);
-
-    }
-};
-
-module.exports = connectToMongo;
+  
