eb501c67714f86d3e2d6c4425f0cfa0a3caebe8a
JaySinroja <jay.sinroja12@gmail.com>
2025-04-24 11:46:16 +0530

    c

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-// Import your components
-import Navbar from './components/Navbar/Navbar';
-import Landing_Page from './components/Landing_Page/Landing_Page';
 import Sign_Up from './components/Sign_Up/Sign_Up';
 import Login from './components/Login/Login';
 
@@ -13,6 +10,7 @@ import Login from './components/Login/Login';
   <Route path="/signup" element={<Sign_Up />} />
   <Route path="/login" element={<Login />} />
 </Routes>
+
 
 
 function App() {
