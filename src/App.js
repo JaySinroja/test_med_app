996e64e9f8762cab388926c14f71359c3aebe131
JaySinroja <jay.sinroja12@gmail.com>
2025-04-25 00:12:35 +0530

    app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar/Navbar';
 import Landing_Page from './components/Landing_Page/Landing_Page';
 import Sign_Up from './components/Sign_Up/Sign_Up';
 import Login from './components/Login/Login';
+import InstantConsultation from './components/InstantConsultationBooking/InstantConsultation'; // ✅ NEW IMPORT
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Landing_Page />} />
         <Route path="/signup" element={<Sign_Up />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/instant" element={<InstantConsultation />} /> {/* ✅ NEW ROUTE */}
       </Routes>
     </BrowserRouter>
   );
