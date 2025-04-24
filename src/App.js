170079bfa411746b140ff0435217797ddf9e8d0c
JaySinroja <jay.sinroja12@gmail.com>
2025-04-24 11:14:38 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+// Import your components
+import Navbar from './components/Navbar/Navbar';
+import Landing_Page from './components/Landing_Page/Landing_Page';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar /> {/* Navbar stays visible on all pages */}
+      <Routes>
+        <Route path="/" element={<Landing_Page />} />
+        {/* You can add more routes later like Sign_Up, Login, etc. */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
