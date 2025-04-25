0500ff9c1bc6a58029fec11e12ef3f2db567a923
JaySinroja <jay.sinroja12@gmail.com>
2025-04-25 11:32:56 +0530

    m

--- src/App.js
+++ src/App.js
@@ -7,38 +7,34 @@ import Navbar from './components/Navbar/Navbar';
 import Landing_Page from './components/Landing_Page/Landing_Page';
 import Sign_Up from './components/Sign_Up/Sign_Up';
 import Login from './components/Login/Login';
-import InstantConsultation from './components/InstantConsultationBooking/InstantConsultation'; //  NEW IMPORT
+import InstantConsultation from './components/InstantConsultationBooking/InstantConsultation';
 import FindDoctorSearch from './components/FindDoctorSearch/FindDoctorSearch';
 import DoctorCard from './components/DoctorCard/DoctorCard';
 
-
-
 function App() {
   return (
-    
     <BrowserRouter>
-      <Navbar /> {/* Navbar stays visible on all pages */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Landing_Page />} />
         <Route path="/signup" element={<Sign_Up />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/instant" element={<InstantConsultation />} /> {/*  NEW ROUTE */}
+        <Route path="/instant" element={<InstantConsultation />} />
         <Route path="/find-doctor" element={<FindDoctorSearch />} />
-        <Route path="/doctor-card" element={<DoctorCard {...sampleDoctor} />} />
-        <div>
-      <DoctorCard
-        name="Dr. Aisha Khan"
-        speciality="Neurologist"
-        experience={10}
-        ratings="4.8"
-      />
-    </div>
-
+        <Route
+          path="/doctor-card"
+          element={
+            <DoctorCard
+              name="Dr. Aisha Khan"
+              speciality="Neurologist"
+              experience={10}
+              ratings="4.8"
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
-    
   );
-  
 }
 
 export default App;
